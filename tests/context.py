69e71717b29c18c59721e69fed099cc626c60134
Axel Uhlig <axel.uhlig@gmail.com>
2020-12-04 18:11:58 +0000

    better tests

---
+++ tests/context.py
@@ -0,0 +1,2 @@
+import sys
+sys.path.append('../github_to_elastic_search')
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,13 @@
 import unittest
+import context
+
+import core as unit
 
 
 class TestCore(unittest.TestCase):
 
     def test_foo(self):
-        self.assertEqual(1, 2)
+        self.assertEqual(1, unit.foo())
 
 
 if __name__ == '__main__':
