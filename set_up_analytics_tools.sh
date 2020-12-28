51970da163cfc836243af6d0f6800f5c64f7dbd6
Axel Uhlig <axel.uhlig@gmail.com>
2020-12-28 16:02:41 +0000

    added ufw commands 3

--- set_up_analytics_tools.sh
+++ set_up_analytics_tools.sh
@@ -9,11 +9,10 @@ docker rm $(docker ps -a -q)
 docker network rm somenetwork
 
 docker network create somenetwork
-docker run -d --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 elasticsearch:7.9.3
+docker run -d --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.9.3
 docker run -d --name kibana --net somenetwork -p 5601:5601 kibana:7.9.3
 
-echo "IP of Kibana (Port 5601): "
-ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p'
+ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p' | xargs bash -c 'echo IP of Kibana: $0:5601'
 
 # Security
-# - block external traffic to server using a firewall: ufw allow from YOUR.PUBLIC.IP && ufw enable
+# - block external traffic to server using a firewall: ufw allow from YOUR.PUBLIC.HOST.IP && ufw enable
