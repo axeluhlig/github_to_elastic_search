4fb5bd9c3c3868a4562aef668a180958b60f702e
Axel Uhlig <axel.uhlig@gmail.com>
2021-03-19 23:32:11 +0000

    wip notes

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Periodically push your git meta data to elasticsearch for further analysis.
 1. `pip3 install -r requirements.txt`
 1. `./main.py`
 
+You can use the `./set_up_analytics_tools.sh` to start up elasticsearch, Kibana and Grafana on your local machine. 
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from configparser import ConfigParser
 
 from github_to_elastic_search import github, elasticsearch
 
+# WiP notes
+# - Use https://docs.github.com/en/rest/reference/repos#get-a-commit on top to get list of modified files
+# - Create list of directories and use https://api.github.com/repos/{owner}/{repo}/commits?sha={branch}&path={path} to query history for each directory for new index
+
 
 def fetch_and_push_for_ever(config, gh):
     waiting_time = int(config.get('general', 'waiting_time_between_cycles_in_seconds'))
