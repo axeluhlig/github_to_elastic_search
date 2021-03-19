e9c3d882b74728d0fc4c0f8b25a24e833e18b828
Axel Uhlig <axel.uhlig@gmail.com>
2021-03-19 21:51:30 +0000

    refactoring and locally green tests

--- github_to_elastic_search/elasticsearch.py
+++ github_to_elastic_search/elasticsearch.py
@@ -3,27 +3,24 @@ from elasticsearch.client import IndicesClient
 
 
 class ElasticSearch():
+
+    COMMIT_INDEX_NAME = 'commits'
+
     def __init__(self, config):
         self.config = config
-        self.index_names = {'commits': 'commits'}
         self.es = ES(hosts=[config.get('elasticsearch', 'url')])
         self.ic = IndicesClient(self.es)
 
     def push_commits(self, commits):
-        self.__ensure_index_exists(self.index_names['commits'])
+        self.__ensure_index_exists(self.COMMIT_INDEX_NAME)
         for commit in commits:
             self.__push_commit(commit)
         print('push of all commits to elasticsearch successful')
 
     def __push_commit(self, commit):
-        self.es.index(
-            index=self.index_names['commits'], id=commit['sha'], body=commit)
+        self.es.index(self.COMMIT_INDEX_NAME, id=commit['sha'], body=commit)
 
     def __ensure_index_exists(self, index_name):
         if not self.ic.exists(index=index_name):
             self.ic.create(index_name)
 
-
-# es.push_commits()
-# -> for every commit
-# -> push to es, using sha as _id
--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -37,52 +37,65 @@ class TestElasticSearch(unittest.TestCase):
         config = configparser.ConfigParser()
         config['elasticsearch'] = {'url': 'localhost:9200'}
         self.unit = elasticsearch.ElasticSearch(config)
+        self.unit.ic = FakeIndicesClient()
+        self.unit.es = FakeElasticSearch()
         super(TestElasticSearch, self).setUp()
 
     def test_push_commits_index_creation_needed(self):
-        fake_indices_client = FakeIndicesClient()
-        self.unit.ic = fake_indices_client
-        self.unit.es = FakeElasticSearch()
-        commits = [{'sha': 'random sha', 'data': 'random data'}]
+        # Arrange
+        commits = [{'sha': 'arbitrary sha', 'data': 'arbitrary data'}]
+
+        # Act
         self.unit.push_commits(commits)
-        self.assertTrue('commits' in fake_indices_client.indices)
+        
+        # Assert
+        self.assertTrue('commits' in self.unit.ic.indices)
 
     def test_push_commits_index_creation_not_needed(self):
-        fake_indices_client = FakeIndicesClient(initial_indices=['commits'])
-        self.unit.ic = fake_indices_client
-        self.unit.es = FakeElasticSearch()
+        # Arrange
+        self.unit.ic = FakeIndicesClient(initial_indices=['commits'])
         commits = [{'sha': 'arbitrary sha', 'data': 'arbitrary data'}]
+        
+        # Act
         self.unit.push_commits(commits)
-        self.assertTrue(fake_indices_client.indices == ['commits'])
+
+        # Assert
+        self.assertTrue(self.unit.ic.indices == ['commits'])
 
     def test_push_commits_no_sha(self):
+        # Arrange
+        self.unit.ic = FakeIndicesClient(initial_indices=['commits'])
         commits = [{'no_sha': 'arbitrary_string', 'data': 'arbitrary data'}]
+        
+        # Act and Assert
         with self.assertRaises(KeyError):
             self.unit.push_commits(commits)
 
     def test_push_commits_new_commit(self):
+        # Arrange
         self.unit.ic = FakeIndicesClient(initial_indices=['commits'])
-        self.unit.es = FakeElasticSearch()
         commits = [{'sha': 'arbitrary sha', 'data': 'arbitrary data'}]
+        expected_els_data = [{'body': {'data': 'arbitrary data', 'sha': 'arbitrary sha'}, 'id': 'arbitrary sha', 'index': 'commits'}]
+        
+        # Act
         self.unit.push_commits(commits)
-        expected_els_data = [{'body': {'data': 'arbitrary data',
-                                       'sha': 'arbitrary sha'}, 'id': 'arbitrary sha', 'index': 'commits'}]
+        
+        # Assert
         self.assertTrue(self.unit.es.data == expected_els_data)
 
     def test_push_commits_already_existing_commit(self):
+        # Arrange
+        self.unit.ic = FakeIndicesClient(initial_indices=['commits'])
         expected_els_data = [{'body': {'data': 'arbitrary data',
                                        'sha': 'arbitrary sha'}, 'id': 'arbitrary sha', 'index': 'commits'}]
-        self.unit.ic = FakeIndicesClient(initial_indices=['commits'])
         self.unit.es = FakeElasticSearch(initial_data=expected_els_data)
         commits = [{'sha': 'arbitrary sha', 'data': 'arbitrary data'}]
+        
+        # Act
         self.unit.push_commits(commits)
-        self.assertTrue(self.unit.es.data == expected_els_data)
 
-    # for debugging only
-    # def test_push_commits_not_faked(self):
-    #     commits = [{'sha': 'arbitrarySha', 'data': 'arbitrary data'}]
-    #     self.unit.push_commits(commits)
-    #     self.assertTrue(1, 1)
+        # Assert
+        self.assertTrue(self.unit.es.data == expected_els_data)
 
 
 if __name__ == '__main__':
